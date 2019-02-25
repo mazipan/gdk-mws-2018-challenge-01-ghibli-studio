3f1353db66a3d5f74969f7cdab5ad8001b4025fa
Irfan Maulana <irfan.maulana@bizzy.co.id>
2019-02-25 19:05:53 +0700

    init mvp layout

--- assets/data.js
+++ assets/data.js
@@ -4,6 +4,13 @@ module.exports = [
     avatar: 'https://avatars2.githubusercontent.com/u/7221389?s=200&v=4',
     github_link: 'https://github.com/mazipan/ghibli-fans',
     demo_link: 'https://mazipan.github.io/ghibli-fans/',
-    technology: ['Preact', 'Redux']
+    technologies: ['Preact', 'Redux']
+  },
+  {
+    name: 'Denny Pradipta',
+    avatar: 'https://avatars3.githubusercontent.com/u/16670475?s=200&v=4',
+    github_link: 'https://github.com/dennydap/mws-ghibli-openapi',
+    demo_link: 'http://dennydap.github.io/mws-ghibli-openapi',
+    technologies: ['React']
   }
 ];
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,44 @@
 <template>
   <section class="container" style="margin: 3em auto;">
-    <div v-for="item in dataRepo"
-      :key="item.name"
-      class="notification is-primary">
-      <figure class="image is-96x96">
-        <img class="is-rounded" :src="item.avatar">
-      </figure>
-      {{ item.name }}
+    <div v-for="item in dataRepo" :key="item.name" class="notification is-primary">
+      <div class="level">
+        <div class="level-left">
+          <figure class="image is-96x96">
+            <img class="is-rounded" :src="item.avatar">
+          </figure>
+        </div>
+        <div class="level-right">
+          <div>
+            <h2 class="subtitle">{{ item.name }}</h2>
+            <div>
+              <div class="tags">
+                <span
+                  v-for="(tech, index) in item.technologies"
+                  :key="index"
+                  class="tag is-warning"
+                  style="margin: 1em .5em;"
+                >{{ tech }}</span>
+              </div>
+            </div>
+            <div style="margin: 1em 0;">
+              <a style="margin: .5em;" alt="Github" :href="item.github_link" target="_blank" rel="noopener">Github</a>
+              <a style="margin: .5em;" alt="Demo" :href="item.demo_link" target="_blank" rel="noopener">Demo</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-import dataRepo from '../assets/data.js'
+import dataRepo from "../assets/data.js";
 
 export default {
-  data () {
+  data() {
     return {
       dataRepo
-    }
+    };
   }
-}
+};
 </script>
