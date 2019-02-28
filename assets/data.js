d366b40c9d1b445dcf374b2162836e0b6eac1019
Irfan Maulana <mazipanneh@gmail.com>
2019-03-01 04:21:34 +0700

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -4,7 +4,7 @@ module.exports = [
     avatar: 'https://avatars2.githubusercontent.com/u/7221389?s=200&v=4',
     github_link: 'https://github.com/mazipan/ghibli-fans',
     demo_link: 'https://mazipan.github.io/ghibli-fans/',
-    technologies: ['Preact', 'Redux']
+    technologies: ['Preact', 'Redux', 'Redux-Thunk']
   },
   {
     name: 'Denny Pradipta',
@@ -18,6 +18,6 @@ module.exports = [
     avatar: 'https://avatars0.githubusercontent.com/u/8471024?s=460&v=4',
     github_link: 'https://github.com/muslimalfatih/ghibli-studio',
     demo_link: 'https://muslimalfatih.github.io/ghibli-studio/',
-    technologies: ['React', 'Redux']
+    technologies: ['React', 'Redux', 'Redux-Thunk', 'TypeScript']
   }
 ];
