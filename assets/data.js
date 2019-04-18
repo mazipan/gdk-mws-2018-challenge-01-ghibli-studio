82ebed24cf4d80001672080a316569d1df21ff4d
Irfan Maulana <mazipanneh@gmail.com>
2019-04-18 18:37:19 +0700

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -5,6 +5,13 @@ module.exports = [
     github_link: 'https://github.com/mazipan/ghibli-fans',
     demo_link: 'https://mazipan.github.io/ghibli-fans/',
     technologies: ['Preact', 'Redux', 'Redux-Thunk']
+  },  
+  {
+    name: 'Irfan Maulana',
+    avatar: 'https://avatars2.githubusercontent.com/u/7221389?s=200&v=4',
+    github_link: 'https://github.com/mazipan/ghibli-reasonreact',
+    demo_link: 'https://mazipan.github.io/ghibli-reasonreact/',
+    technologies: ['ReasonReact']
   },
   {
     name: 'Denny Pradipta',
