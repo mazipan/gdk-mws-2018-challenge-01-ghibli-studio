adbcb6ed0f974b3bf0eab78ea7f7c50500a68c91
Irfan Maulana <irfan.maulana@bizzy.co.id>
2019-04-18 15:22:57 +0700

    add badge

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,23 +5,33 @@
         <div class="container">
           <div class="level" style="display: flex;flex-direction: column;">
             <img src="/icons/favicon-96x96.png" alt="Logo Ghibli Apps">
-            <h1 class="title">
-              Ghibli Apps
-            </h1>
+            <h1 class="title">Ghibli Apps</h1>
             <br>
-            <h2 class="subtitle">
-              👾 Google Developer Kejar Mobile Web Specialist 2018 - Challenge 01 - Playing with Ghibli Studio Public API
-            </h2>
+            <h2
+              class="subtitle"
+            >👾 Google Developer Kejar Mobile Web Specialist 2018 - Challenge 01 - Playing with Ghibli Studio Public API</h2>
+            <div>
+              <a
+                href="https://github.com/mazipan/gdk-mws-2018-challenge-01-ghibli-studio"
+                target="_blank"
+                rel="noopener noreferer"
+              >
+                <img
+                  alt="GitHub Stars"
+                  src="https://img.shields.io/github/stars/mazipan/gdk-mws-2018-challenge-01-ghibli-studio.svg?style=social"
+                >
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </section>
-    <nuxt />
+    <nuxt/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DefaultLayout'
-}
+  name: "DefaultLayout"
+};
 </script>
