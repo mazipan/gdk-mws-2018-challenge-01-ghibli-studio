91616af3ed2e4e593872cda010e687673b33f25a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-18 11:39:33 +0000

    Deploy mazipan/gdk-mws-2018-challenge-01-ghibli-studio to github.com/mazipan/gdk-mws-2018-challenge-01-ghibli-studio.git:gh-pages

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/05e9d9d90ac37a449f0e.js",
-    "revision": "fada7281af3286ae522a2aa8bdff6aea"
+    "url": "/_nuxt/5a814f3f0930a859bb57.js",
+    "revision": "9d1cd53a077ece56e7c7c203c44dd9cd"
   },
   {
-    "url": "/_nuxt/7d2152bce3b677ddabf4.js",
-    "revision": "e678cfa1c229f82266372ea3be058738"
+    "url": "/_nuxt/9747c248d0fe92f6f6ac.js",
+    "revision": "dbfd805e9eb33ef608d90890af0ace29"
   },
   {
     "url": "/_nuxt/a13df40e88a027585202.js",
